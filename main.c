2a273e058628e2161ed86593e3a624caaac648dc
zerrinayz <zerrinayaz76@gmail.com>
2024-11-06 21:50:55 +0300

    program main norm corrected

--- main.c
+++ main.c
@@ -6,17 +6,46 @@
 /*   By: zayaz <zayaz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:25:29 by zayaz             #+#    #+#             */
-/*   Updated: 2024/11/06 17:12:37 by zayaz            ###   ########.fr       */
+/*   Updated: 2024/11/06 21:50:00 by zayaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//patlıyor echo "$USER$USERasd$PWD"
+// patlıyor echo "$USER$USERasd$PWD"
+char	*get_user_input(t_program *program)
+{
+	char	*tmp;
+
+	program->finish_check = 0;
+	g_global_signal = 0;
+	program->input = readline("minishell 🐥>");
+	if (g_global_signal == 13)
+	{
+		program->status = 1;
+		g_global_signal = 0;
+	}
+	tmp = ft_strtrim(program->input, " ");
+	free(program->input);
+	program->input = tmp;
+	if (program->input)
+		add_history(program->input);
+	if (program->input == NULL)
+		exit(1);
+	return (tmp);
+}
+
+static void	free_main_program(t_program *program, char *tmp)
+{
+	free_parser_input(program);
+	free(tmp);
+	free(program->process);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
-	t_program *program;
-	char *tmp;
+	t_program	*program;
+	char		*tmp;
 
 	program = NULL;
 	(void)argv;
@@ -31,29 +60,11 @@ int	main(int argc, char **argv, char **envp)
 	ft_init_program(program, envp);
 	while (1)
 	{
-		program->finish_check = 0;
-		g_global_signal = 0;
-		program->input = readline("minishell 🐥>");
-		if (g_global_signal == 13)
-		{
-			program->status = 1;
-			g_global_signal = 0;
-		}
-		tmp = ft_strtrim(program->input, " ");
-		free(program->input);
-		program->input = tmp;
-		if (program->input)
-			add_history(program->input);
-		if (program->input == NULL)
-			exit(1);
+		tmp = get_user_input(program);
 		if (!ft_parser(program, program->input))
 			continue ;
 		zi_exec(program);
-		free_parser_input(program);
-		free(tmp);
-	//	free(program->here_fd);
-		free(program->process);
+		free_main_program(program, tmp);
 	}
 	free_program(program);
-	// system("leaks minishell");
 }
