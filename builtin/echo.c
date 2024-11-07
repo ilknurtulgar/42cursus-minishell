8937b71e00638135326c74db317b3f43656ebb8a
zerrinayz <zerrinayaz76@gmail.com>
2024-11-07 17:19:45 +0300

    echo -n flag edited

--- builtin/echo.c
+++ builtin/echo.c
@@ -6,18 +6,32 @@
 /*   By: zayaz <zayaz@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 17:36:01 by itulgar           #+#    #+#             */
-/*   Updated: 2024/11/06 13:43:44 by zayaz            ###   ########.fr       */
+/*   Updated: 2024/11/07 17:17:55 by zayaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int	flag_helper(char *cmd, char c)
+{
+	if (*cmd == '\0')
+		return (0);
+	while (*cmd)
+	{
+		if (*cmd != c)
+			return (0);
+		cmd++;
+	}
+	return (1);
+}
+
 static int	flag_control(char **cmd, int *i)
 {
-	if (cmd[1][0] == '-' && cmd[1][1] == 'n' && cmd[1][2] == '\0')
+	if (cmd[1][0] == '-' && cmd[1][1] == 'n')
 	{
-		while (cmd[*i] && zi_strcmp(cmd[*i], "-n") == 0)
+		while (cmd[*i] && cmd[*i][0] == '-' && flag_helper(&cmd[*i][1], 'n'))
 		{
+			printf("girdim\n");
 			if (cmd[*i])
 				(*i)++;
 		}
